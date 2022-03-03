b6801291641d5d555413dcf9c51a1625119144e4
Andronomos <andronomos@inbox.com>
2022-03-02 20:38:50 -0800

    Create ColorStore.cs

---
+++ Color Generator/Stores/ColorStore.cs
@@ -0,0 +1,36 @@
+﻿using Color_Generator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Color_Generator.Stores
+{
+    public class ColorStore : ObservableObject
+    {
+        public List<UserColor> Colors { get; } = new List<UserColor>();
+
+        public int Total => Colors.Count;
+
+        public event EventHandler ColorsChanged;
+
+        public void AddColor(UserColor color)
+        {
+            Colors.Add(color);
+            UpdateStore();
+        }
+
+        public void ClearColors()
+        {
+            Colors.Clear();
+            UpdateStore();
+        }
+
+        private void UpdateStore()
+        {
+            ColorsChanged?.Invoke(this, null);
+            OnPropertyChanged(nameof(Total));
+        }
+    }
+}
